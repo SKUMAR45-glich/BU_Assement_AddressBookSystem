d3324605f9e48d29eeb2d257b4db0ae21726e2a9
Saurabh <saurabh.nit20@gmail.com>
2020-11-24 09:49:55 +0530

    [Saurabh].Create AddressBook

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        string _name;
+        public AddressBook()
+        {
+            this._name = "General";
+        }
+
+        public AddressBook(string name)
+        {
+            this._name = name;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,9 +4,59 @@ namespace AddressBookSystem
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Address Book");
+            Console.WriteLine("========================");
+
+            AddressBook addressBook = new AddressBook();
+            addressBook.Name = "General";
+
+            bool checkAddressBook = true;
+            bool checkContactDetails = true; ;
+
+
+            while (checkAddressBook)
+            {
+                Console.WriteLine("Enter\n" +
+                "1 : To Add a new Address Book\n" +
+                "2 : To use current address books ( " + addressBook.Name + " )\n" +
+                "3 : Switch Address Book\n" +
+                "0 : Exit");
+
+                int userChoice = Int32.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        checkContactDetails = true;
+                        Console.WriteLine("Add Name of the new Address Book");
+                        addressBook.Name = Console.ReadLine();
+                        break;
+                    case 2:
+                        checkContactDetails = true;
+                        break;
+                    case 3:
+                        checkContactDetails = true;
+                        Console.WriteLine("Enter Name of the Address Book you want to switch");
+                        addressBook.Name = Console.ReadLine();
+                        break;
+                    case 0:
+                        checkAddressBook = false;
+                        checkContactDetails = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Option Entered");
+                        break;
+                }
+
+                while (checkContactDetails)
+                {
+                    Console.WriteLine("You are in " + addressBook.Name);
+                    checkContactDetails = false;
+
+                }
+            }
         }
     }
 }
